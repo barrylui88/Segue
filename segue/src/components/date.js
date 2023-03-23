76022d69b5898bf6ab23d13ad4562ec5eb39301f
bcl <barrylui88@googlemail.com>
2023-03-23 19:06:53 +0000

    Date fix

--- segue/src/components/date.js
+++ segue/src/components/date.js
@@ -5,7 +5,7 @@ const dayjs = require('dayjs')
 //import dayjs from 'dayjs' // ES 2015
 const date = dayjs().get('date')
 const month = dayjs().get('month')
-const year = dayjs().get('year');
+const year = dayjs().get('year')
 
 function Date () {
     return (<h1>{date}/{month}/{year}</h1>)
