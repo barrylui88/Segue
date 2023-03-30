d4ef67fd6d0b8832be762ca458c51dd715a0a68a
bcl <barrylui88@googlemail.com>
2023-03-30 18:03:16 +0100

    Added React Type Animation NPM

--- segue/src/components/footer.js
+++ segue/src/components/footer.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
-function Footer() {
-    return(
-    <div>
-        <p className="middle">World Autism Acceptance Week 27 March - 2 April 2023</p>
-    </div>
+const Footer = () => {
+    return (
+      <TypeAnimation
+        sequence={[
+          'Did you know?', // Types 'One'
+          1000, // Waits 1s
+          'It\'s World Autism Acceptance Week',
+          2000, // Waits 2s
+          '27 March - 2 April 2023',
+          () => {
+            console.log('Sequence completed'); // Place optional callbacks anywhere in the array
+          }
+        ]}
+        wrapper="span"
+        cursor={true}
+        repeat={Infinity}
+        style={{ fontSize: '2em', display: 'inline-block' }}
+      />
     );
-  }
+  };
+
+// function Footer() {
+//     return(
+//     <div>
+//         <p className="middle">World Autism Acceptance Week 27 March - 2 April 2023</p>
+//     </div>
+//     );
+//   }
   
   export default Footer;
